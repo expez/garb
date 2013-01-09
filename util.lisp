b2006ef75eb7d8302a7b6a14ef4bb0d9530a4e52
Expez <mrgrandmother@gmail.com>
2013-01-09 15:54:09 +0100

    Add util.lisp and implement elementp.

---
+++ util.lisp
@@ -0,0 +1,6 @@
+(defpackage util)
+
+(defun elementp (e)
+  "Returns True if e is testable with EQL, i.e. e is of type
+  character, number or package."
+  (or (characterp e) (numberp e) (packagep e)))
