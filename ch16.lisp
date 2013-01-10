6e0790c7bd874d7de15a164ba1dbcc175a2fd761
Expez <mrgrandmother@gmail.com>
2013-01-10 09:08:31 +0100

    (defun my-count)

--- ch16.lisp
+++ ch16.lisp
@@ -8,3 +8,10 @@
   (check-type e1 (satisfies util:elementp))
   (check-type e1 (satisfies util:elementp))
   (> (length (member e1 l)) (length (member e2 l))))
+
+(defun my-count (e l)
+  "Returns the number of occurrences of e in l"
+  (check-type l list)
+  (cond ((null l) 0)
+        ((eql (first l) e) (1+ (my-count e (rest l))))
+        ('t (my-count e (rest l)))))
