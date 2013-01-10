af3cda18330507fc33f008d973f1c954ab4882ee
Expez <mrgrandmother@gmail.com>
2013-01-10 08:56:29 +0100

    rewrite before

--- ch16.lisp
+++ ch16.lisp
@@ -7,6 +7,4 @@
   "Returns True if the element e1 occurs before the element e2 in the list l"
   (check-type e1 (satisfies util:elementp))
   (check-type e1 (satisfies util:elementp))
-  (cond ((null l) nil)
-        ((eql (first l) e1) 't)
-        ('t (before e1 e2 (rest l)))))
+  (> (length (member e1 l)) (length (member e2 l))))
